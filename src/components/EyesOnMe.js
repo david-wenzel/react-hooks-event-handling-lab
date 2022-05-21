8ea41a67a31ba0f78639bc1e7d4f71ba12b008fd
David Wenzel <dwenzel95@gmail.com>
2022-05-21 16:00:02 -0500

    commit tree 37403463eb594be77076959ea866a8567a3bd3e6

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import react from "react";
+
+function handleBlur(){
+    console.log('Hey! Eyes on me!')
+}
+
+
+function handleFocus(){
+    console.log('Good!')
+}
+
+
+function EyesOnMe(){
+    return <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+
+import react from "react";
+
+function handlePass(){
+    console.log('Entering password...')
+}
+
+function Keypad(){
+    return(
+     <input type="password" onChange={handlePass}/>
+    )
+}
+
+export default Keypad
